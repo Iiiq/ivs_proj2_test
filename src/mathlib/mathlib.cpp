33457754fdeec22c494dbf56d1fff97303f2f4c5
Igor <1iiiq2001@gmail.com>
2021-02-14 13:09:12 +0100

    Pridani zdrojovych souboru matematicke knihovny Vytvoreny prototypy pro vsechny funkce. Jsou naimplementovane funkce: addition, subtraction

---
+++ src/mathlib/mathlib.cpp
@@ -0,0 +1,35 @@
+
+extern "C" double addition(double a, double b)
+{
+    return a + b;
+}
+
+extern "C" double subtraction(double a, double b)
+{
+    return a - b;
+}
+
+double division(double a, double b)
+{
+    return 0;
+}
+
+double multiplication(double a, double b)
+{
+    return 0;
+}
+
+double factorial(double a)
+{
+    return 0;
+}
+
+double eponentiation(double a, double n)
+{
+    return 0;
+}
+
+double nthroot(double a, double n)
+{
+    return 0;
+}
